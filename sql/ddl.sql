3d51e154a8334d0d5f2bcaebe7d345d894f5da81
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-09-11 09:54:37 -0400

    Initial commit

---
+++ sql/ddl.sql
@@ -0,0 +1,17 @@
+create table corps (
+  lei                            text primary key,
+  lname                          text not null,
+  jurisdiction                   text,
+  legal_form                     text not null,
+  entity_status                  text not null,
+  reg_status                     text not null,
+  init_reg                       date not null,
+  next_renewal                   date not null,
+  last_update                    timestamp with time zone not null,
+  managing_lou                   text not null foreign key references corps);
+
+create table securities (
+      lei       text,
+      isin      text,
+      primary key (lei, isin),
+      foreign key (lei) references corps);
