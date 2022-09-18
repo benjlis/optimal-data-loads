cf7216a2cd7a509e4f96bba439464d67ec708e30
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-09-17 23:42:47 -0400

    Renamed to cp.sql and removed fk references to managing_lou

--- cp.sql
+++ cp.sql
@@ -1,12 +1,11 @@
+\timing
 \copy companies from 'companies.csv' CSV
 -- drop PK/FK constraints....and any additional indexes
-alter table companies drop constraint companies_managing_lou_fkey;
 alter table companies drop constraint companies_pkey;
 -- COPY companies data
 \copy companies from 'companies.csv' CSV
 -- recreate PK/FK constraints
 alter table companies add primary key (lei);
-alter table companies add foreign key (managing_lou) references companies;
 -- disable logging
 alter table companies set unlogged;
 \copy companies from 'companies.csv' CSV
